cdab14589be5496034198eae3d65f3260ade0091
DanielCornejo <dcornejo@applaudostudios.com>
2021-07-04 10:10:51 -0600

    :tada: Begins project

---
+++ Ravn-Challenge-V2-DanielCornejo/Ravn_Challenge_V2_DanielCornejoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ravn_Challenge_V2_DanielCornejoApp.swift
+//  Ravn-Challenge-V2-DanielCornejo
+//
+//  Created by Daniel Cornejo on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct Ravn_Challenge_V2_DanielCornejoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
