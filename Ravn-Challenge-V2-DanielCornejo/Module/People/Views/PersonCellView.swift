c240f49b3f48a04558621de880eee365518167da
DanielCornejo <dancor08@gmail.com>
2021-07-04 21:05:42 -0600

    :construction: Adds NavigationLink to PersonViewCell

--- Ravn-Challenge-V2-DanielCornejo/Module/People/Views/PersonCellView.swift
+++ Ravn-Challenge-V2-DanielCornejo/Module/People/Views/PersonCellView.swift
@@ -8,26 +8,26 @@
 import SwiftUI
 
 struct PersonCellView: View {
-    
-    let name: String
-    let description: String
+    let person: Person
     
     var body: some View {
-        ZStack {
-            VStack {
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text(name)
-                            .modifier(TextStyle.h2Default)
-                        Text(description)
-                            .modifier(TextStyle.p1LowEmphasis)
+        NavigationLink(destination: PersonDetailView(person: person)) {
+            ZStack {
+                VStack {
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(person.name ?? "")
+                                .modifier(TextStyle.h2Default)
+                            Text(person.shortDescription)
+                                .modifier(TextStyle.p1LowEmphasis)
+                        }
+                        Spacer()
+                        Image(systemName: "chevron.right")
                     }
-                    Spacer()
-                    Image(systemName: "chevron.right")
+                    .padding(16.0)
+                    Divider()
+                        .padding(.leading, 16.0)
                 }
-                .padding(16.0)
-                Divider()
-                    .padding(.leading, 16.0)
             }
         }
     }
@@ -35,7 +35,15 @@ struct PersonCellView: View {
 
 struct PersonCellView_Previews: PreviewProvider {
     static var previews: some View {
-        PersonCellView(name: "Daniel Cornejo", description: "Human for El Salvador")
+        PersonCellView(person: Person(birthYear: "12May92",
+                                      eyeColor: "Brown",
+                                      hairColor: "Black",
+                                      homeworld: Homeworld(id: "001", name: "El Salvador"),
+                                      id: "04654235-9",
+                                      name: "Daniel Cornejo",
+                                      skinColor: "Brunette",
+                                      species: Species(id: "002", name: nil),
+                                      vehicleConnection: Vehicles(vehicles: [Vehicle(id: "000001", name: "Hyundai Elantra"), Vehicle(id: "000002", name: "Toyota Yaris")])))
             .previewLayout(.sizeThatFits)
     }
 }
